4f5ac36024cf54e4b5f60270881e1ca08cc3dac8
Noah Okada <nssokada@gmail.com>
2024-11-22 14:03:01 -0800

    test2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-LimaTaskMain-0.1";
 const contentToCache = [
-    "Build/limabuild_1122.loader.js",
-    "Build/limabuild_1122.framework.js",
-    "Build/limabuild_1122.data",
-    "Build/limabuild_1122.wasm",
+    "Build/limabuild_1123.loader.js",
+    "Build/limabuild_1123.framework.js",
+    "Build/limabuild_1123.data",
+    "Build/limabuild_1123.wasm",
     "TemplateData/style.css"
 
 ];
